7bffd552e71db1a7ee7a84ec859ce52f81b8b0c3
Sebastian Wick <wick.sebastian@gmail.com>
2012-09-18 01:29:41 +0200

    complete rewrite + irssi patch

---
+++ README.md
@@ -0,0 +1,57 @@
+# irssi-passwd
+the script receives passwords from other scripts (like gnomekeyring.py) and uses them to connect to a server or in any command
+
+## warning
+The passwd.pl script needs a modified version if irssi. Apply the patch irssi-connection-set-key.patch, then compile and install.
+
+    cd path/to/irssi_src
+    patch -p1 < path/to/irssi-connection-set-key.patch
+    ./autogen.sh
+    make
+    sudo make install
+
+
+## install
+move passwd.pl to ~/.irssi/scripts
+create a symlink in ~/.irssi/scripts/autorun/ to ~/.irssi/scripts/passwd.pl
+
+## configure
+The default config file is ~/.irssi/passwd and can be changed with the setting config_file in the passwd section.
+In the config file you can store a command which will print the password for the password\_id to stdout.
+The password\_id ([a-zA-Z0-9_\\-]+) and the command are seperated by a : (colon), each pair is seperated by a newline:
+
+    example_id      : echo "mypassword"
+    example_keyring : python ~/.irssi/scripts/gnomekeyring.py mykeyring nameofthekey
+
+
+## use
+
+### /passwd
+
+    /passwd password_id irc_command
+
+will replace the string &lt;password> in irc\_command with the password received from the command associated with password\_id. irc\_command is executed at the end.
+
+    /passwd example_id /echo <password>
+
+The script looks up the command associated with example\_id in the config file (echo "mypassword"), execute it and replace &lt;password> with the output ("mypassword").
+The result (/echo "mypassword") is executed.
+
+    /passwd example_keyring /msg NickServ identify <password>
+
+Here the script will identify your nick with the password from the gnome keyring.
+
+### irssi server config 
+
+If the script is loaded it will automatically replace &lt;password:password\_id> in the server config with the value received from to command associated with password\_id
+when the server is connecting.
+
+    {
+      address = "irc.freenode.net";
+      chatnet = "Freenode";
+      password = "user:<password:example_id>";
+      autoconnect = "yes";
+    }
+
+Assuming the following config, irssi will automatically connect to irc.freenode.net, replace "user:&lt;password:example\_id>" with "user:mypassword" and login.
+
---
+++ passwd.pl
@@ -0,0 +1,115 @@
+use strict;
+
+use IPC::Open3;
+use Irssi;
+use Symbol;
+
+
+my $passwords;
+my $help_passwd = '
+PASSWD <passwd_id> <command>
+
+  Replaces the string \'<password>\' with the password from passwd_id in command and executes the result.
+  The passwd_id\'s are defined in \'~/.irssi/passwd\' (or in the file specified by the setting config_file in the section passwd).
+  The format of the password file is \'passwd_id:command\\n\' where command writes the password to stdout
+';
+
+Irssi::settings_add_str('passwd', 'config_file', "$ENV{HOME}/.irssi/passwd");
+passwd_init();
+
+sub passwd_init {
+  my $cfg_file = Irssi::settings_get_str('config_file');
+  open my $handle, $cfg_file or return;
+
+  my %cfg;
+  while(<$handle>) {
+    my $line = $_;
+    if($line =~ /^\s*([a-zA-Z0-9_\-]+)\s*:(.*)$/) {
+      my $key = $1;
+      my $value = $2;
+      $value =~ s/^\s*//;
+      $cfg{$key} = $value;
+      next;
+    }
+    if($line =~ /^\s*$/) {
+      next;
+    }
+    Irssi::print("syntax error in file $cfg_file:$.", MSGLEVEL_CLIENTCRAP);
+  }
+
+  close $handle;
+  $passwords = \%cfg;
+}
+
+sub passwd_get_password {
+  my ($pw_key) = @_;
+  my ($stdin, $stdout, $stderr);
+  my $cmd = $passwords->{$pw_key};
+  if(!$cmd) {
+    Irssi::print("No command to get password $pw_key", MSGLEVEL_CLIENTCRAP);
+    return;
+  }
+
+  $stderr = gensym();
+  my $pid = open3($stdin, $stdout, $stderr, $cmd);
+  waitpid($pid, 0);
+
+  if($? != 0) {
+    my $error = join('', <$stderr>);
+    chomp($error);
+    Irssi::print("Couldn't get password: $error", MSGLEVEL_CLIENTCRAP);
+    return;
+  }
+
+  my $pw = join('', <$stdout>);
+  chomp($pw);
+  return $pw;
+}
+
+Irssi::signal_add_first('server connecting', sub {
+  my ($server, @rest) = @_;
+  if($server->{password} =~ /\<password:([a-zA-Z0-9]+)\>/) {
+    my $password = passwd_get_password($1);
+    $server->{password} =~ s/\<password:[a-zA-Z0-9]+\>/$password/;
+    Irssi::Server::connection_set_key($server, 'password', $server->{password});
+  }
+  Irssi::signal_continue($server, @rest);
+});
+
+Irssi::command_bind('passwd', sub {
+  my ($args) = @_;
+  my @argv = split(/ /, $args);
+  my $argc = @argv;
+  if($argc < 2) {
+    Irssi::print('Too few arguments', MSGLEVEL_CLIENTCRAP);
+    return;
+  }
+  my $cmd = join(' ', @argv[1..$argc-1]);
+  my $pw = passwd_get_password(@argv[0]);
+  $cmd =~ s/\<password\>/$pw/;
+  Irssi::command($cmd);
+});
+
+Irssi::command_bind('help', sub {
+  if($_[0] =~ /^\s*passwd\s*$/) {
+    Irssi::print($help_passwd, MSGLEVEL_CLIENTCRAP);
+    Irssi::signal_stop;
+  }
+});
+
+Irssi::signal_add_first('complete word', sub {
+  my ($strings, $window, $word, $linestart, $want_space) = @_;
+
+  return if $linestart ne '/passwd';
+
+  foreach my $key (keys %$passwords) {
+    my $sub = substr($key, 0, length($word));
+    if($sub eq $word) {
+      push(@$strings, $key);
+      $$want_space = 1;
+    }
+  }
+
+  Irssi::signal_stop;
+});
+
